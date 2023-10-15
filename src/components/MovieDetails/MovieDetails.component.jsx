c8e395315e07bc85a2811a005cf5af5e3e9cde2a
Stefan Gogov <stefangogov@proton.me>
2023-10-16 00:12:37 +0300

    Changed API call

--- src/components/MovieDetails/MovieDetails.component.jsx
+++ src/components/MovieDetails/MovieDetails.component.jsx
@@ -71,7 +71,7 @@ export default function MovieDetails({
       try {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
         );
 
         if (!res.ok)
